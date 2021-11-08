100c3cc32d6e391a5c77c03dad7446785216ba2a
JC <sdbook@gmail.com>
2021-11-08 09:12:25 +0800

    seesion and cookie example

---
+++ seesion and cookie/session-get.php
@@ -0,0 +1,33 @@
+<?php
+	//Session 使用範例
+	session_start(); //啟用session 功能, 必須在php程式還沒輸出任何訊息之前啟用
+	//$_SESSION["varName"] = "session ok"; //宣告session 變數並指定值
+
+	if ( isset($_SESSION["keyValue"])) {
+		echo "Session['keyValue']=" . $_SESSION["keyValue"] . "<br>";
+	} else {
+		echo "Session['keyValue'] does not exist!<br>";
+	}
+
+	if ( isset($_COOKIE["keyValue"])) {
+		echo "cookie['keyValue']=" . $_COOKIE["keyValue"] . "<br>";
+	} else {
+		echo "cookie['keyValue'] does not exist!<br>";
+	}
+
+/*
+	$a = $_SESSION["keyValue"]; //取得session 變數 keyValue 的值
+	unset($_SESSION['變數名稱']); //取消session 變數的宣告
+
+	//cookie使用範例
+	setcookie("cookieName", "cookieValue", time()+36000); // 設定cookie值與有效時間
+	$b= $_COOKIE["cookieName"]; //取得cookie的值
+	echo $a, $b;
+
+*/
+?>
+<hr>
+<form method="post" action="session-set.php">
+Input Key value <input type="text" name="key">
+<input type="submit">
+</form>
---
+++ seesion and cookie/session-set.php
@@ -0,0 +1,9 @@
+<?php
+	session_start(); //啟用session 功能, 必須在php程式還沒輸出任何訊息之前啟用
+	$val = $_POST["keyValue"];
+	
+	$_SESSION["keyValue"] = $val; //宣告session 變數並指定值
+	setcookie("keyVallue", $val, time()+36000); // 設定cookie值與有效時間
+?>
+ok!!
+<a href="session-get.php">get session value</a>
