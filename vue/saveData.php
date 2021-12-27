97a806b7048c5473622300dabab5cdba5faa8938
JC <sdbook@gmail.com>
2021-12-27 09:44:45 +0800

    minor fix

--- vue/saveData.php
+++ vue/saveData.php
@@ -4,7 +4,7 @@ $id=(int)$_POST['i'];
 //this is for the first way to get the posted stringify json data
 $dataStr = $_POST['dat'];
 $data = json_decode($dataStr);
-
+//$data['name']
 /*
 //this is for another way to get the post data without json stringify
 //this is the traditional way
