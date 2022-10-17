0190f7602768bc6ee5d701373e2dfef632281b86
DESKTOP-ONV3A21\buddhist <sdbook@gmail.com>
2022-10-17 23:48:54 +0800

    B

--- 000.php
+++ 000.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+BB
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>文件A</title>
+<title>文件BB</title>
 
 </head>
 add by a
