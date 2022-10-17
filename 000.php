942516d69de2824c284ed8aafc9a6dd4926eaffd
DESKTOP-ONV3A21\buddhist <sdbook@gmail.com>
2022-10-17 23:22:12 +0800

    u

--- 000.php
+++ 000.php
@@ -2,7 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<<<<<<< HEAD
 <title>文件1 edit a</title>
+=======
+<title>文件1 edit b</title>
+>>>>>>> origin/master
 </head>
 add by a
 <body>
